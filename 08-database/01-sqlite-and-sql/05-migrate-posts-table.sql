12a5590ce72fb700ed2beae8c17fb5254eab3174
Drew Guarnera <dguarnera@wooster.edu>
2025-10-21 09:00:45 -0400

    Update 05-migrate-posts-table.sql

--- 08-database/01-sqlite-and-sql/05-migrate-posts-table.sql
+++ 08-database/01-sqlite-and-sql/05-migrate-posts-table.sql
@@ -1,7 +1,5 @@
---INSERT INTO posts (author_id, title, content, created_at)
 SELECT 
 	a.id as author_id,
-	---a.username,
 	r.post_title as title,
 	r.post_content as content,
 	r.created_at as created_at
